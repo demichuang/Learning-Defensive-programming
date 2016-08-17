e5be9886800a2056e45b5bc2a700d917291c23a8
hi <demichuang40207@gmail.com>
2016-08-17 03:09:18 +0000

    structure almost done

--- try.php
+++ try.php
@@ -11,11 +11,26 @@ class Daemon
             exit;
         }
 
+        // become session leader
         posix_setsid();
 
         if ($pid) {
             exit;
         }
+
+        $this->echoSomething();
+
+        return $this;
     }
 
+    public function echoSomething()
+    {
+        for ($i = 0; $i < 5; $i++) {
+            echo "child pid " . posix_getpid() . "\n";
+        }
+    }
 }
+
+$http = new Daemon();
+
+$http->start();
